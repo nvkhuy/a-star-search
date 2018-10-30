4eb563ce4bddf5a4610322ed02ca6999649776d8
nguyenvukhanhhuy <khanhhuynguyenvu@gmail.com>
2018-10-30 21:38:34 +0700

    update 30/10/2018

--- Project/MachineLearningBFS.py
+++ Project/MachineLearningBFS.py
@@ -22,37 +22,52 @@ def CreateShape(x,y,Time,color):
 	Frame.create_rectangle(5+distx, 5+disty, N-5 +distx, N-5 +disty, outline='white', fill=color)
 	Frame.update()
 #Event Origin
-def getorigin(eventorigin):
+def dragOrigin(eventorigin):
       x = eventorigin.x
       y = eventorigin.y
       x = int(x/N)
       y = int(y/N)
       if(x == RunCell[0] and y == RunCell[1]):
-      	print(BFS())
-      	print("Nguyen Vu Khanh Huy")
-      	print("MSSV: 16025591")
-      	print("DHKHMT12A")
-      #Continuty Click Start Cell
-      elif(Start[0] == -1 and Start[1] == -1):
-      	Start[0] = x
-      	Start[1] = y
-      	CreateShape(x,y,0,"Red")
+      	return 0
       elif(x == Start[0] and y == Start[1]):
-      	CreateShape(Start[0],Start[1],0,"white")
-      	Start[0] = -1
-      	Start[1] = -1
-      #Continuty Click End Cell
-      elif(End[0] == -1 and End[1] == -1):
-      	End[0] = x
-      	End[1] = y
-      	CreateShape(x,y,0,"Orange")
+      	return 0
       elif(x == End[0] and y == End[1]):
-      	CreateShape(End[0],End[1],0,"white")
-      	End[0] = -1
-      	End[1] = -1
+      	return 0
       else:
       	Block[x][y] = 1
       	CreateShape(x,y,0,"pink")
+#startBFS
+def getorigin(eventorigin):
+	x = eventorigin.x
+	y = eventorigin.y
+	x = int(x/N)
+	y = int(y/N)
+
+	if(Start[0]!= -1):
+		if(End[0]!=-1):
+			if(x == RunCell[0] and y == RunCell[1]):
+				flag = BFS()
+				print(flag)
+				if(flag == True):
+					traceBack()
+					print(dist[End[0]][End[1]])
+			elif(x == Start[0] and y == Start[1]):
+				CreateShape(Start[0],Start[1],0,"white")
+				Start[0] = -1
+				Start[1] = -1
+			elif(x == End[0] and y == End[1]):
+				CreateShape(End[0],End[1],0,"white")
+				End[0] = -1
+				End[1] = -1
+		elif(x != Start[0] or y != Start[1]):
+			End[0] = x
+			End[1] = y
+			CreateShape(x,y,0,"Orange")
+	elif(x != End[0] or y != End[1]):
+		Start[0] = x
+		Start[1] = y
+		CreateShape(x,y,0,"Red")	
+
 #Init Grid
 def InitMatrix():
 	#Gobal varaiable
@@ -65,7 +80,9 @@ def InitMatrix():
 	Frame = tk.Canvas(root, height=1500, width=1500, bg='white')
 	Frame.pack(fill=tk.BOTH, expand=True)
 	Frame.bind('<Configure>', create_grid)
+	# Frame.bind("<Button 1>",getorigin)
 	Frame.bind("<Button 1>",getorigin)
+	Frame.bind("<B1-Motion>",dragOrigin)
 	#Start Cell: color Red
 	Start = ([5,6])
 	CreateShape(Start[0],Start[1],0,"Red")
@@ -74,11 +91,28 @@ def InitMatrix():
 	CreateShape(End[0],End[1],0,"Orange")
 	#Run Cell: color Red
 	global RunCell
-	RunCell = ([49,0])
+	RunCell = ([20,0])
 	CreateShape(RunCell[0],RunCell[1],0,"Red")
+#printTrack
+def traceBack():
+	x = End[0]
+	y = End[1]
+	a = path[x][y][0]
+	b = path[x][y][1]
+	while(a!=-1 and b!=-1):
+		if(x != End[0] or y != End[1]):
+			CreateShape(x,y,0,"Purple")
+		x = a
+		y = b
+		a = path[x][y][0]
+		b = path[x][y][1]
+
 #BFS
 def BFS():
 	BaseTime = 0
+	global path,dist
+	path = [[([]) for i in range(sizeX+10)] for i in range(sizeY+10)]
+	dist = [[0 for i in range(sizeX+10)] for i in range(sizeY+10)]
 	#direct up, right, down, left
 	dx = [0,1,0,-1]
 	dy = [-1,0,1,0]
@@ -86,6 +120,7 @@ def BFS():
 	used = [[0 for i in range(sizeX+10)] for i in range(sizeY+10)]
 	#Queue
 	q = queue.Queue(maxsize = sizeX*sizeY+10)
+	path[Start[0]][Start[1]] = ([-1,-1])
 	q.put([Start[0],Start[1]])
 	while(q.empty() == False):
 		Front = q.get()
@@ -109,6 +144,8 @@ def BFS():
 				if(X>=0 and X <= sizeX and Y>=0 and Y<=sizeY):
 					#print('Co',X,Y,used[X][Y])
 					#CreateShape(X,Y,"blue")
+					dist[X][Y] = dist[Front[0]][Front[1]]+1
+					path[X][Y] = ([Front[0],Front[1]])
 					q.put([X,Y])
 	return False		
 #Main
